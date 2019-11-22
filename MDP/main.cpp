fc046e17043ab04f362089c01814f7395033d21d
Andres <andres.bd@outlook.com>
2019-11-21 21:05:30 -0600

    Problem at index of max

--- MDP/main.cpp
+++ MDP/main.cpp
@@ -8,10 +8,13 @@
 
 #include <iostream>
 #include <sstream>
+#include <vector>
+#include <iterator>
 #include "cCell.hpp"
 
 #define MDP_WIDTH 3
 #define MDP_HEIGHT 2
+#define GAMMA 0.9
 
 using namespace std;
 
@@ -20,7 +23,9 @@ Cell*** prevValues = NULL;    // 2D array of *Cell
 Cell*** currPolicy = NULL;    // 2D array of *Cell
 Cell*** prevPolicy = NULL;    // 2D array of *Cell
 // rewards is the R in MDP=<S,A,T,R>
-Cell*** rewards = NULL;        // 2D array of *Cell
+Cell*** rewards = NULL;       // 2D array of *Cell
+
+float residium = 1-GAMMA;
 
 ostringstream iterStream(ostringstream::ate);
 unsigned int iteration;
@@ -46,12 +51,83 @@ bool inBounds(int row, int col)
     return row >= 0 && row < MDP_HEIGHT && col >= 0 && col < MDP_WIDTH;
 }
 
-/*
+int findNeighbours(int r, int c, int directions[], float nei[]) {
+    int cont = 0;
+    
+    if(inBounds(c-1, r+0) != false) {cont++; directions[0] = 1; nei[0] = rewards[c-1][r]->fValue;} //UP
+    if(inBounds(c-1, r+1) != false) {cont++; directions[1] = 1; nei[1] = rewards[c-1][r+1]->fValue;} //RU
+    if(inBounds(c+0, r+1) != false) {cont++; directions[2] = 1; nei[2] = rewards[c][r+1]->fValue;} //R
+    if(inBounds(c+1, r+1) != false) {cont++; directions[3] = 1; nei[3] = rewards[c+1][r+1]->fValue;} //RD
+    if(inBounds(c+1, r+0) != false) {cont++; directions[4] = 1; nei[4] = rewards[c+1][r]->fValue;} //D
+    if(inBounds(c+1, r-1) != false) {cont++; directions[5] = 1; nei[5] = rewards[c+1][r-1]->fValue;} //LD
+    if(inBounds(c+0, r-1) != false) {cont++; directions[6] = 1; nei[6] = rewards[c][r-1]->fValue;} //L
+    if(inBounds(c-1, r-1) != false) {cont++; directions[7] = 1; nei[7] = rewards[c-1][r-1]->fValue;} //LU
+    nei[8] = rewards[c][r]->fValue;
+    
+    return cont;
+}
+
+void calculateValues(int directions[], float nei[], float div, int index, float max) {
+    
+    std::vector<float> values{-20.0f,-20.0f,-20.0f,-20.0f,-20.0f,-20.0f,-20.0f,-20.0f,-20.0f};
+    
+    for (int i = 0; i < 9; i++) {
+        
+        bool stay = false;
+        int cont = 0;
+        float sum = 0;
+        
+        for (int j = 0; j < 9; j++) {
+            if(i == j) {
+                if(directions[j] == 0) {
+                    stay = true;
+                    continue;
+                }else {
+                    sum += GAMMA * nei[j];
+                }
+            }else {
+                if (j == 8 && stay == true)
+                {
+                    stay = false;
+                    sum += GAMMA * nei[j];
+                }else {
+                    if(directions[j] == 0)  {
+                        continue;
+                    }else {
+                        sum += div * nei[j];
+                    }
+                }
+            }
+        }
+        values[cont] = sum;
+        cont++;
+    }
+    max = *max_element(values.begin(), values.end());
+    index = *distance(values, max);
+    
+    cout << index;
+}
+
 // Perform the Value Iteration using the Bellman equation:
 void valueIteration()
 {
+    for (int i=0; i < MDP_WIDTH; i++) {
+        for (int j=0; j < MDP_HEIGHT; j++) {
+            
+            int directions[9] = {0,0,0,0,0,0,0,0,1};
+            float neighbour[9] ={0,0,0,0,0,0,0,0,0};
+            
+            int found = findNeighbours(i,j, directions, neighbour);
+            float div = residium / (found+1);
+            
+            int index;
+            float max;
+            
+            calculateValues(directions, neighbour, div, index, max);
+        }
+    }
 }
-*/
+
 
 // Print a message, followed by a table:
 void showTable(string msg, Cell*** table)
@@ -89,7 +165,7 @@ void clean()
     delete currPolicy;
     delete prevPolicy;
     delete rewards;
-    cout << endl << "All clean. Bye." << endl;
+    cout << endl << "All clean." << endl;
 }
 
 // Compute the MDP solution (don't forget to write a file with the optimal policy):
@@ -119,7 +195,7 @@ int main()
             rewards[r][c] = new Cell(Cell::CT_REWARD);        // Instance of the rewards cell
             rewards[r][c]->row = r;
             rewards[r][c]->column = c;
-            rewards[r][c]->fValue = -0.03;                    // Default cost for just moving
+            rewards[r][c]->fValue = -0.04;                    // Default cost for just moving
         }
     }
 
@@ -130,6 +206,7 @@ int main()
     copyTable(currPolicy, prevPolicy);                        // Update the previous policy
 
     showIteration();
+    valueIteration();
     /*
         ToDo:
         while(!convergence)
